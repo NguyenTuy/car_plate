f322464c82b4bd9a8667a89b07c119efa9fef9de
NguyenTuy <tuy.nv@homecredit.vn>
2019-12-16 17:07:36 +0700

    Module

--- DetectPlateV2.py
+++ DetectPlateV2.py
@@ -1,6 +1,5 @@
 import cv2
-import numpy as np
-from lib_detection import load_model, detect_lp, im2single
+from lib_detection import detect_lp, im2single
 
 # Dinh nghia cac ky tu tren bien so
 char_list = '0123456789ABCDEFIGHKLMNPRSTUVXYZ'
@@ -17,48 +16,47 @@ def fine_tune(lp):
 
 # Đường dẫn ảnh, các bạn đổi tên file tại đây để thử nhé
 img_path = "testdata/1.jpg"
+def get_plate_area(path, wpod_net):
 
-# Load model LP detection
-wpod_net_path = "wpod-net_update1.json"
-wpod_net = load_model(wpod_net_path)
+    # Đọc file ảnh đầu vào
+    Ivehicle = cv2.imread(img_path)
 
-# Đọc file ảnh đầu vào
-Ivehicle = cv2.imread(img_path)
+    # Kích thước lớn nhất và nhỏ nhất của 1 chiều ảnh
+    Dmax = 608
+    Dmin = 288
 
-# Kích thước lớn nhất và nhỏ nhất của 1 chiều ảnh
-Dmax = 608
-Dmin = 288
+    # Lấy tỷ lệ giữa W và H của ảnh và tìm ra chiều nhỏ nhất
+    ratio = float(max(Ivehicle.shape[:2])) / min(Ivehicle.shape[:2])
+    side = int(ratio * Dmin)
+    bound_dim = min(side, Dmax)
 
-# Lấy tỷ lệ giữa W và H của ảnh và tìm ra chiều nhỏ nhất
-ratio = float(max(Ivehicle.shape[:2])) / min(Ivehicle.shape[:2])
-side = int(ratio * Dmin)
-bound_dim = min(side, Dmax)
+    _, LpImg, lp_type = detect_lp(wpod_net, im2single(Ivehicle), bound_dim, lp_threshold=0.5)
 
-_, LpImg, lp_type = detect_lp(wpod_net, im2single(Ivehicle), bound_dim, lp_threshold=0.5)
+    if (len(LpImg)):
+        # Chuyen doi anh bien so
+        LpImg[0] = cv2.convertScaleAbs(LpImg[0], alpha=(255.0))
+        # cv2.imshow("normal", LpImg[0])
 
-if (len(LpImg)):
-    # Chuyen doi anh bien so
-    LpImg[0] = cv2.convertScaleAbs(LpImg[0], alpha=(255.0))
-    # cv2.imshow("normal", LpImg[0])
+        # Chuyen anh bien so ve gray
+        gray = cv2.cvtColor(LpImg[0], cv2.COLOR_BGR2GRAY)
+        # cv2.imshow("gray", gray)
 
-    # Chuyen anh bien so ve gray
-    gray = cv2.cvtColor(LpImg[0], cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("gray", gray)
+        # h, w = gray.shape
+        # height = np.int (h)
+        # width = np.int (w)
+        # print("w-h: %f - %f" %(w, h))
+        # if w < h * 1.5:
+        #     top_image = gray[0: np.int(height / 2), 0: width]
+        #     bottom_image = gray[np.int (height / 2): height, 0: width]
+        #     gray = np.concatenate((top_image, bottom_image), axis=1)
 
-    # h, w = gray.shape
-    # height = np.int (h)
-    # width = np.int (w)
-    # print("w-h: %f - %f" %(w, h))
-    # if w < h * 1.5:
-    #     top_image = gray[0: np.int(height / 2), 0: width]
-    #     bottom_image = gray[np.int (height / 2): height, 0: width]
-    #     gray = np.concatenate((top_image, bottom_image), axis=1)
-
-    # Ap dung threshold de phan tach so va nen
-    binary = cv2.threshold(gray, 127, 255,
-                           cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    # binary = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
-    # cv2.imshow("threshold", binary)
-    # kernel = np.ones((5,5),np.uint8)
-    # binary = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel)
-    # cv2.imshow("close", binary)
+        # Ap dung threshold de phan tach so va nen
+        binary = cv2.threshold(gray, 127, 255,
+                               cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        # binary = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
+        # cv2.imshow("threshold", binary)
+        # kernel = np.ones((5,5),np.uint8)
+        # binary = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel)
+        # cv2.imshow("close", binary)
+        return binary
+    return LpImg[0]
--- PredictCharacters.py
+++ PredictCharacters.py
@@ -1,70 +1,81 @@
 import SegmentCharacters
+import DetectPlateV2
 import pickle
-import cv2
-from random import randint
+from lib_detection import load_model
+
 test_data = []
 test_data.append(("testdata/1.jpg", "30A56999"))
-test_data.append(("testdata/2.jpg", "37A02656"))
-test_data.append(("testdata/3.jpg", "22L8288"))
-test_data.append(("testdata/4.jpg", "95L3456"))
-test_data.append(("testdata/5.jpg", "18A00402"))
-test_data.append(("testdata/6.jpg", "30F24442"))
-test_data.append(("testdata/7.jpg", "MCLRNF1"))
-test_data.append(("testdata/8.jpg", "MH01AV8866"))
-test_data.append(("testdata/9.jpg", "48A56789"))
-test_data.append(("testdata/10.jpg", "15A22222"))
-test_data.append(("testdata/11.jpg", "76A07676"))
-test_data.append(("testdata/12.jpg", "51F21111"))
-test_data.append(("testdata/13.jpg", "51F22160"))
-test_data.append(("testdata/14.jpg", "30A77178"))
-test_data.append(("testdata/15.jpg", "35A00135"))
-test_data.append(("testdata/16.jpg", "51A33888"))
-test_data.append(("testdata/17.jpg", "51A12288"))
-test_data.append(("testdata/18.jpg", "51F55295"))
-test_data.append(("testdata/19.jpg", "89A08888"))
-test_data.append(("testdata/20.jpg", "89A09196"))
-test_data.append(("testdata/21.jpg", "T1596455"))
-test_data.append(("testdata/22.jpg", "37A20700"))
-test_data.append(("testdata/23.jpg", "T1576549"))
-test_data.append(("testdata/24.jpg", "61T33727"))
-test_data.append(("testdata/25.jpg", "43A44444"))
-test_data.append(("testdata/26.jpg", "38P105694"))
-test_data.append(("testdata/27.jpg", "70C105314"))
-test_data.append(("testdata/28.jpg", "50N155555"))
-test_data.append(("testdata/29.jpg", "59D277777"))
-test_data.append(("testdata/30.jpg", "29U102222"))
+# test_data.append(("testdata/2.jpg", "37A02656"))
+# test_data.append(("testdata/3.jpg", "22L8288"))
+# test_data.append(("testdata/4.jpg", "95L3456"))
+# test_data.append(("testdata/5.jpg", "18A00402"))
+# test_data.append(("testdata/6.jpg", "30F24442"))
+# test_data.append(("testdata/7.jpg", "MCLRNF1"))
+# test_data.append(("testdata/8.jpg", "MH01AV8866"))
+# test_data.append(("testdata/9.jpg", "48A56789"))
+# test_data.append(("testdata/10.jpg", "15A22222"))
+# test_data.append(("testdata/11.jpg", "76A07676"))
+# test_data.append(("testdata/12.jpg", "51F21111"))
+# test_data.append(("testdata/13.jpg", "51F22160"))
+# test_data.append(("testdata/14.jpg", "30A77178"))
+# test_data.append(("testdata/15.jpg", "35A00135"))
+# test_data.append(("testdata/16.jpg", "51A33888"))
+# test_data.append(("testdata/17.jpg", "51A12288"))
+# test_data.append(("testdata/18.jpg", "51F55295"))
+# test_data.append(("testdata/19.jpg", "89A08888"))
+# test_data.append(("testdata/20.jpg", "89A09196"))
+# test_data.append(("testdata/21.jpg", "T1596455"))
+# test_data.append(("testdata/22.jpg", "37A20700"))
+# test_data.append(("testdata/23.jpg", "T1576549"))
+# test_data.append(("testdata/24.jpg", "61T33727"))
+# test_data.append(("testdata/25.jpg", "43A44444"))
+# test_data.append(("testdata/26.jpg", "38P105694"))
+# test_data.append(("testdata/27.jpg", "70C105314"))
+# test_data.append(("testdata/28.jpg", "50N155555"))
+# test_data.append(("testdata/29.jpg", "59D277777"))
+# test_data.append(("testdata/30.jpg", "29U102222"))
+
+def get_plate(model, column_list, characters):
+    print('Model loaded. Predicting characters of number plate')
+    classification_result = []
+    for each_character in characters:
+        # converts it to a 1D array
+        each_character = each_character.reshape(1, -1);
+        result = model.predict(each_character)
+        classification_result.append(result)
+
+        plate_string = ''
+        for eachPredict in classification_result:
+            plate_string += eachPredict[0]
+
+        print('Predicted license plate')
+        print(plate_string)
+
+        # it's possible the characters are wrongly arranged
+        # since that's a possibility, the column_list will be
+        # used to sort the letters in the right order
+
+        column_list_copy = column_list[:]
+        column_list.sort()
+        rightplate_string = ''
+        for each in column_list:
+            rightplate_string += plate_string[column_list_copy.index(each)]
+
+        # print('License plate')
+        # print(rightplate_string)
+        return rightplate_string
 
 print("Loading model")
 filename = './finalized_model.sav'
 model = pickle.load(open(filename, 'rb'))
 
-print('Model loaded. Predicting characters of number plate')
-classification_result = []
-for each_character in SegmentCharacters.characters:
-    # converts it to a 1D array
-    each_character = each_character.reshape(1, -1);
-    result = model.predict(each_character)
-    classification_result.append(result)
+wpod_net_path = "wpod-net_update1.json"
+wpod_net = load_model(wpod_net_path)
 
-print('Classification result')
-print(classification_result)
+for test in test_data:
+    binary = DetectPlateV2.get_plate_area(test[0], wpod_net)
+    column_list, characters = SegmentCharacters.get_number_area(binary)
+    number = get_plate(model, column_list, characters)
+    print("Check file %s which is %s - predict is %s" %(test[0], test[1], number))
 
-plate_string = ''
-for eachPredict in classification_result:
-    plate_string += eachPredict[0]
 
-print('Predicted license plate')
-print(plate_string)
-
-# it's possible the characters are wrongly arranged
-# since that's a possibility, the column_list will be
-# used to sort the letters in the right order
-
-column_list_copy = SegmentCharacters.column_list[:]
-SegmentCharacters.column_list.sort()
-rightplate_string = ''
-for each in SegmentCharacters.column_list:
-    rightplate_string += plate_string[column_list_copy.index(each)]
-
-print('License plate')
-print(rightplate_string)
